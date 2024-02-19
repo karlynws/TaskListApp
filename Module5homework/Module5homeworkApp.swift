5c04b68a383ebcdbafb87d43975a669d2584200b
karlynws <146772646+karlynws@users.noreply.github.com>
2024-02-19 12:11:34 -0700

    Initial Commit

---
+++ Module5homework/Module5homeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Module5homeworkApp.swift
+//  Module5homework
+//
+//  Created by Karlyn Sanders on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Module5homeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
