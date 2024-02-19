d440acdc70f07bd66fa488ac7d1b4a862baf3a3c
karlynws <146772646+karlynws@users.noreply.github.com>
2024-02-19 16:42:53 -0700

    Basic outline, a good start

---
+++ Module5homework/TaskDetailsPage.swift
@@ -0,0 +1,18 @@
+//
+//  TaskDetailsPage.swift
+//  Module5homework
+//
+//  Created by Karlyn Sanders on 2/19/24.
+//
+
+import SwiftUI
+
+struct TaskDetailsPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskDetailsPage()
+}
