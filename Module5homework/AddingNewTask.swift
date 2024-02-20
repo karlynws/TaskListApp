f28363788825954918eb0be014990c66811fcd73
karlynws <146772646+karlynws@users.noreply.github.com>
2024-02-20 10:51:34 -0700

    Added New Task file and buttons.

---
+++ Module5homework/AddingNewTask.swift
@@ -0,0 +1,18 @@
+//
+//  AddingNewTask.swift
+//  Module5homework
+//
+//  Created by Karlyn Sanders on 2/20/24.
+//
+
+import SwiftUI
+
+struct AddingNewTask: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddingNewTask()
+}
